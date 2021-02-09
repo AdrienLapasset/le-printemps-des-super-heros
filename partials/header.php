6eaca2eeb968b728c2e962a177f72e2da9a6fc2d
Adrien Lapasset <aflapasset@gmail.com>
2021-02-09 17:55:44 +0100

    init

--- partials/header.php
+++ partials/header.php
@@ -1,5 +1,5 @@
 <header>
-  <div class="d-flex d-md-none align-items-center mb-3">
+  <!-- <div class="d-flex d-md-none align-items-center mb-3">
     <picture>
       <img class="mr-2" src="assets/logos/logo-ckdb.png" alt="">
     </picture>
@@ -9,5 +9,5 @@
   </div>
   <img class="d-none d-md-block" src="assets/logos/logo-ckdb.png" alt="">
   <img src="assets/imgs/jouez-cest-que-du-bonheur.png" alt="">
-  <img class="d-none d-md-block" src="assets/logos/logo-hasbro.png" alt="">
+  <img class="d-none d-md-block" src="assets/logos/logo-hasbro.png" alt=""> -->
 </header>
--- teaser.php
+++ teaser.php
@@ -5,21 +5,19 @@
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
   <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
-  <title>Le Printemps des Super-Héros</title>
+  <title>CEKEDUBONHEUR</title>
   <meta name="description" content="">
   <link rel="stylesheet" href="modules/bootstrap-4.5.3/dist/css/bootstrap.min.css">
   <link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
 
 <body>
-  <div class="container d-flex align-items-center justify-content-center" style="height: 100vh;">
-    <div class="w-100">
-      <h1>Pages :</h1>
-      <div class="list-group">
-        <a class="list-group-item list-group-item-action" href="teaser.php">Teaser</a>
-      </div>
-    </div>
-  </div>
+  <?php include 'partials/header.php'; ?>
+  <main class="teaser">
+    <img src="../assets/imgs/jouez-avec-les-super-heros.png" alt="">
+    <h1>Le printemps des super-héros</h1>
+    <img src="../assets/imgs/gagner-400€-sombre.png" alt="">
+  </main>
 </body>
 
 </html>
