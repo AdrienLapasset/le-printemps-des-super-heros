33dd7b53c2a0c0c9b30ded07f6a0600e3388426b
Adrien Lapasset <aflapasset@gmail.com>
2021-02-11 10:22:33 +0100

    add page confirmation

---
+++ confirmation.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
+  <title>Le printemps des super-héros</title>
+  <meta name="description" content="">
+  <link rel="stylesheet" href="modules/bootstrap-4.5.3/dist/css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+
+<body class="gagne">
+  <?php include 'partials/header.php'; ?>
+    <img src="../assets/imgs/le-printemps-des-super-heros.png" alt="">
+    <h1>Merci d’avoir participé</h1>
+    <p class="mb-3">Votre dossier est en cours de validation.
+      Un email va vous être envoyé prochainement.<p>
+    <p>
+      Découvrez tous nos jouets de Super-Héros
+      BEYBLADE BURST, Power Rangers©, Transformers©, Star Wars™ et Marvel© !</p>
+    <a href="/formulaire.html" class="c-btn">Découvrir</a>
+    <a href="/formulaire.html" class="c-btn yellow">revenir à l’accueil</a>
+  <?php include 'partials/footer-printemps.php'; ?>
+</body>
+
+</html>
