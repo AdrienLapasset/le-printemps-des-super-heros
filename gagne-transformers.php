d78dc13ad465044ef1a776ace4bcf09548d70c22
Adrien Lapasset <aflapasset@gmail.com>
2021-02-11 10:07:55 +0100

    add gagne pages

---
+++ gagne-transformers.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
+  <title>Le printemps des super-héros</title>
+  <meta name="description" content="">
+  <link rel="stylesheet" href="modules/bootstrap-4.5.3/dist/css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+
+<body class="gagne">
+  <?php include 'partials/header.php'; ?>
+    <img src="../assets/imgs/le-printemps-des-super-heros.png" alt="">
+    <h1>Bravo !</h1>
+    <p>Vous avez gagné un jouet Transformers©</p>
+    <img src="../assets/imgs/gagne-transformers.png" alt="">
+    <aside>*Plusieurs modèles disponibles - Visuels non contractuels</aside>
+    <a href="/formulaire.html" class="c-btn">Continuer</a>
+
+  <?php include 'partials/footer-printemps.php'; ?>
+</body>
+
+</html>
